8cd82f44269f063f64a020842e8e4d8f90d1ac89
Sike.Chen <sike.chen@outlook.com>
2017-02-06 19:00:18 +0800

    wall

---
+++ SourceCode/IKCoder/App_Code/Defined/class_HonorWallItems.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class class_HonorWallItems
+{
+    public string Computerprofessor
+    {
+        get
+        {
+             return "computerprofessor";
+        }
+    }
+
+    public string Shareexpert
+    {
+        get
+        {
+            return "shareexpert";
+        }
+    }
+
+    public string Mathematician
+    {
+        get
+        {
+            return "mathematician";
+        }
+    }
+
+    public string Arithmeticprofessor
+    {
+        get
+        {
+            return "arithmeticprofessor";
+        }
+    }
+    
+    public string Musician
+    {
+        get
+        {
+            return "musician";
+        }
+    }
+
+    public string Sciencemastermind
+    {
+        get
+        {
+            return "sciencemastermind";
+        }
+    }
+
+    public string Littlepinter
+    {
+        get
+        {
+            return "littlepinter";
+        }
+    }
+
+    public string UAVpilot
+    {
+        get
+        {
+            return "UAVpilot";
+        }
+    }
+
+}
