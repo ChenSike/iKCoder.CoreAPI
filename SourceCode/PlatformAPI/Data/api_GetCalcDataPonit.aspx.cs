5eb4974d3bbf66886e4eb1c515a69d6e387e2a4f
sike.chen <sike.chen@morningstar.com>
2017-03-24 13:55:52 +0800

    new code

--- SourceCode/PlatformAPI/Data/api_GetCalcDataPonit.aspx.cs
+++ SourceCode/PlatformAPI/Data/api_GetCalcDataPonit.aspx.cs
@@ -21,16 +21,45 @@ public partial class Data_api_GetCalcDataPonit : class_WebClass_WA
             Dictionary<string, XmlDocument> dataSource_Buffer = new Dictionary<string, XmlDocument>();
             Dictionary<string, double> dataPoint_Buffer = new Dictionary<string, double>();
             XmlNode sourceNode = REQUESTDOCUMENT.SelectSingleNode("/root/source");
-            foreach(XmlNode sourceItemNode in sourceNode.SelectNodes("item"))
+            XmlNode defindNode = REQUESTDOCUMENT.SelectSingleNode("/root/defined");
+            class_Data_SqlSPEntry activeSPEntry = object_CommonLogic.GetActiveSP(object_CommonLogic.dbServer, "spa_operation_data_basic");
+            class_Data_SqlHelper _objectSqlHelper = new class_Data_SqlHelper();           
+            foreach (XmlNode sourceItemNode in sourceNode.SelectNodes("item"))
             {
+                string symbol = class_XmlHelper.GetAttrValue(sourceItemNode, "symbol");
+                string id = class_XmlHelper.GetAttrValue(sourceItemNode, "id");
+                activeSPEntry.ClearAllParamsValues();                
+                activeSPEntry.ModifyParameterValue("@symbol", symbol);
+                DataTable textDataTable = _objectSqlHelper.ExecuteSelectSPConditionForDT(activeSPEntry, object_CommonLogic.Object_SqlConnectionHelper, object_CommonLogic.dbServer);
+                if(textDataTable!=null && textDataTable.Rows.Count > 0)
+                {
+                    string strDoc = "";
+                    class_Data_SqlDataHelper.GetArrByteColumnDataToString(textDataTable.Rows[0], "data", out strDoc);
+                    XmlDocument resultDoc = new XmlDocument();
+                    resultDoc.LoadXml(strDoc);
+                    dataSource_Buffer.Add(id, resultDoc);
+                }
+            }
+            foreach(XmlNode definedItemNode in defindNode.SelectNodes("item"))
+            {
+                string source = class_XmlHelper.GetAttrValue(definedItemNode, "source");
+                string id = class_XmlHelper.GetAttrValue(definedItemNode, "id");
+                string value = class_XmlHelper.GetAttrValue(definedItemNode, "value");
+                if (string.IsNullOrEmpty(value))
+                {
 
+                }
+                else
+                {
+
+                }
             }
 
 
-            class_Data_SqlSPEntry activeSPEntry = object_CommonLogic.GetActiveSP(object_CommonLogic.dbServer, "spa_operation_data_basic");
-            class_Data_SqlHelper _objectSqlHelper = new class_Data_SqlHelper();
-            
-            DataTable textDataTable = _objectSqlHelper.ExecuteSelectSPConditionForDT(activeSPEntry, object_CommonLogic.Object_SqlConnectionHelper, object_CommonLogic.dbServer);
+
+
+
+
             if (textDataTable != null && textDataTable.Rows.Count > 0)
             {
                 string result = "";
