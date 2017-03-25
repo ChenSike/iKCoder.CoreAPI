5dac7f85b2842e717c5807c5d5d7a6486b8e35e1
Sike.Chen <sike.chen@outlook.com>
2017-03-26 01:52:19 +0800

    new code and new files

---
+++ SourceCode/IKCoder/Data/GET_DataPonit.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GET_DataPonit.aspx.cs" Inherits="Data_GET_DataPonit" %>
--- SourceCode/PlatformAPI/Data/api_GetCalcDataPonit.aspx.cs
+++ SourceCode/PlatformAPI/Data/api_GetCalcDataPonit.aspx.cs
@@ -23,15 +23,15 @@ public partial class Data_api_GetCalcDataPonit : class_WebClass_WA
             XmlNode sourceNode = REQUESTDOCUMENT.SelectSingleNode("/root/source");
             XmlNode defindNode = REQUESTDOCUMENT.SelectSingleNode("/root/defined");
             class_Data_SqlSPEntry activeSPEntry = object_CommonLogic.GetActiveSP(object_CommonLogic.dbServer, "spa_operation_data_basic");
-            class_Data_SqlHelper _objectSqlHelper = new class_Data_SqlHelper();           
+            class_Data_SqlHelper _objectSqlHelper = new class_Data_SqlHelper();
             foreach (XmlNode sourceItemNode in sourceNode.SelectNodes("item"))
             {
                 string symbol = class_XmlHelper.GetAttrValue(sourceItemNode, "symbol");
                 string id = class_XmlHelper.GetAttrValue(sourceItemNode, "id");
-                activeSPEntry.ClearAllParamsValues();                
+                activeSPEntry.ClearAllParamsValues();
                 activeSPEntry.ModifyParameterValue("@symbol", symbol);
                 DataTable textDataTable = _objectSqlHelper.ExecuteSelectSPConditionForDT(activeSPEntry, object_CommonLogic.Object_SqlConnectionHelper, object_CommonLogic.dbServer);
-                if(textDataTable!=null && textDataTable.Rows.Count > 0)
+                if (textDataTable != null && textDataTable.Rows.Count > 0)
                 {
                     string strDoc = "";
                     class_Data_SqlDataHelper.GetArrByteColumnDataToString(textDataTable.Rows[0], "data", out strDoc);
@@ -40,36 +40,68 @@ public partial class Data_api_GetCalcDataPonit : class_WebClass_WA
                     dataSource_Buffer.Add(id, resultDoc);
                 }
             }
-            foreach(XmlNode definedItemNode in defindNode.SelectNodes("item"))
+            foreach (XmlNode definedItemNode in defindNode.SelectNodes("item"))
             {
                 string source = class_XmlHelper.GetAttrValue(definedItemNode, "source");
                 string id = class_XmlHelper.GetAttrValue(definedItemNode, "id");
                 string value = class_XmlHelper.GetAttrValue(definedItemNode, "value");
+                string xpath = class_XmlHelper.GetAttrValue(definedItemNode, "xpath");
                 if (string.IsNullOrEmpty(value))
                 {
-
+                    if (dataSource_Buffer.ContainsKey(source))
+                    {
+                        XmlDocument activeSourceDocument = dataSource_Buffer[source];
+                        XmlNode valueNode = activeSourceDocument.SelectSingleNode(xpath);
+                        if (valueNode != null)
+                        {
+                            value = class_XmlHelper.GetNodeValue(valueNode);
+                        }
+                        else
+                        {
+                            AddErrMessageToResponseDOC(class_CommonDefined._Faild_Execute_Api + "Data_api_GetCalcDataPonit", "Wrong Xpath.", "");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        AddErrMessageToResponseDOC(class_CommonDefined._Faild_Execute_Api + "Data_api_GetCalcDataPonit", "Wrong Source.", "");
+                        return;
+                    }
+                }
+                double dValue = 0.0;
+                double.TryParse(value, out dValue);
+                dataPoint_Buffer.Add(id, dValue);
+            }
+            XmlNode calcNode = REQUESTDOCUMENT.SelectSingleNode("/root/calc");
+            if (calcNode != null)
+            {
+                string calcExp = class_XmlHelper.GetNodeValue(calcNode);
+                if (!string.IsNullOrEmpty(calcExp))
+                {
+                    foreach (string activeID in dataPoint_Buffer.Keys)
+                    {
+                        if(calcExp.Contains(activeID))
+                        {
+                            string value = dataPoint_Buffer[activeID].ToString();
+                            calcExp = calcExp.Replace(activeID, value);
+                        }
+                    }
+                    DataTable tmpDataTableObject = new DataTable();
+                    object computeResult = tmpDataTableObject.Compute(calcExp, "");
+                    AddResponseMessageToResponseDOC(class_CommonDefined._Executed_Api + this.GetType().FullName, class_CommonDefined.enumExecutedCode.executed.ToString(), computeResult.ToString(), "");
                 }
                 else
                 {
-
+                    AddErrMessageToResponseDOC(class_CommonDefined._Faild_Execute_Api + "Data_api_GetCalcDataPonit", "No CalcExpr.", "");
+                    return;
                 }
-            }
-
-
-
-
-
-
-            if (textDataTable != null && textDataTable.Rows.Count > 0)
-            {
-                string result = "";
-                class_Data_SqlDataHelper.GetArrByteColumnDataToString(textDataTable.Rows[0], "data", out result);
-                AddResponseMessageToResponseDOC(class_CommonDefined._Executed_Api + this.GetType().FullName, class_CommonDefined.enumExecutedCode.executed.ToString(), result, "");
             }
             else
             {
-                AddErrMessageToResponseDOC(class_CommonDefined._Faild_Execute_Api + "Data_api_GetMetaText", "No data.", "");
+                AddErrMessageToResponseDOC(class_CommonDefined._Faild_Execute_Api + "Data_api_GetCalcDataPonit", "No CalcNode.", "");
+                return;
             }
+
         }
         else
             AddErrMessageToResponseDOC(class_CommonDefined._Faild_Execute_Api + "Data_api_GetMetaText", "No Input.", "");
