de01332dc2e347f8e8bc1c6e8ff520bbcc9689f7
sike.chen <sike.chen@outlook.com>
2017-04-16 13:05:26 +0800

    new code

---
+++ SourceCode/IKCoder/Bus/Workspace/SET_CurrentStep.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iKCoder_Platform_SDK_Kit;
+
+public partial class Bus_Workspace_SET_CurrentStep : class_WebBase_UA
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
