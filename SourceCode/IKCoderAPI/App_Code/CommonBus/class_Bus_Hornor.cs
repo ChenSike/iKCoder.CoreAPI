5a581b779e8a9b28a182c5eb0db4a7c5c29ccb71
sike.chen <sike.chen@outlook.com>
2017-05-02 16:32:25 +0800

    new code

---
+++ SourceCode/IKCoderAPI/App_Code/CommonBus/class_Bus_Hornor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using iKCoder_Platform_SDK_Kit;
+using System.Data;
+
+/// <summary>
+/// Summary description for class_Bus_Hornor
+/// </summary>
+public class class_Bus_Hornor
+{
+
+    public const string symbol_hornor_config = "data_list_hornor";
+
+    protected XmlDocument sourceDoc_hornor = new XmlDocument();
+    protected XmlNodeList nodesList_centerHornor;
+
+    public void init_SourceDoc(class_CommonData Object_CommonData)
+    {
+        string result = string.Empty;
+        class_Data_SqlSPEntry activeSPEntry_configSence = Object_CommonData.GetActiveSP(Object_CommonData.dbServer, class_SPSMap.SP_OPERATION_RESOURCE_TEXT);
+        activeSPEntry_configSence.ClearAllParamsValues();
+        activeSPEntry_configSence.ModifyParameterValue("@symbol", symbol_hornor_config);
+        DataTable textDataTable = Object_CommonData.Object_SqlHelper.ExecuteSelectSPConditionForDT(activeSPEntry_configSence, Object_CommonData.Object_SqlConnectionHelper, Object_CommonData.dbServer);
+        if (textDataTable != null && textDataTable.Rows.Count > 0)
+        {
+            DataRow activeDataRow = null;
+            class_Data_SqlDataHelper.GetActiveRow(textDataTable, 0, out activeDataRow);
+            string data = string.Empty;
+            class_Data_SqlDataHelper.GetColumnData(activeDataRow, "data", out data);
+            data = class_CommonUtil.Decoder_Base64(data);
+            sourceDoc_hornor.LoadXml(data);
+            nodesList_centerHornor = sourceDoc_hornor.SelectNodes("/root/centerhornor/item");
+        }
+    }
+
+    public List<Dictionary<string, string>> get_HornorConfig(List<string> finishedSymbol)
+    {
+        List<Dictionary<string, string>> returnList = new List<Dictionary<string, string>>();
+        if (nodesList_centerHornor != null)
+        {
+            foreach (XmlNode item in nodesList_centerHornor)
+            {
+                Dictionary<string, string> hornorItem = new Dictionary<string, string>();
+                bool isActive = false;
+                string condition = class_XmlHelper.GetAttrValue(item, "condition");
+                foreach (string tmpSymbol in finishedSymbol)
+                {
+                    if (condition.Contains(tmpSymbol))
+                    {
+                        isActive = true;
+                        break;
+                    }
+                }
+                string name = class_XmlHelper.GetAttrValue(item, "name");
+                string active = class_XmlHelper.GetAttrValue(item, "active");
+                string inactive = class_XmlHelper.GetAttrValue(item, "inactive");
+                hornorItem.Add("name", name);
+                hornorItem.Add("image", isActive ? active : inactive);
+                returnList.Add(hornorItem);
+            }           
+
+        }
+        return returnList;
+    }
+
+    public class_Bus_Hornor()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
+
--- SourceCode/IKCoderAPI/Bus/Center/api_iKCoder_Center_Get_CenterInfo.aspx.cs
+++ SourceCode/IKCoderAPI/Bus/Center/api_iKCoder_Center_Get_CenterInfo.aspx.cs
@@ -6,13 +6,115 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using iKCoder_Platform_SDK_Kit;
 using System.Xml;
+using System.Data;
 
-public partial class Bus_Center_api_iKCoder_Center_Get_CenterInfo : class_WebBase_IKCoderAPI_NUA
+public partial class Bus_Center_api_iKCoder_Center_Get_CenterInfo : class_WebBase_IKCoderAPI_UA
 {
     XmlDocument centerDocument = new XmlDocument();
+    XmlDocument sourceDoc_sence = new XmlDocument();
+    XmlDocument sourceDoc_profile = new XmlDocument();
 
+    List<string> symbolList = new List<string>();
+    List<Dictionary<string, string>> honorResult = new List<Dictionary<string, string>>();
+    XmlNode rootNode;
+    int count_primerSence = 0;
+    int count_primarySence = 0;
+    int count_middleSence = 0;
+    int count_seniorSence = 0;
+    int count_advanceSence = 0;
+    int count_primerSence_Complete = 0;
+    int count_primarySence_Complete = 0;
+    int count_middleSence_Complete = 0;
+    int count_seniorSence_Complete = 0;
+    int count_advanceSence_Complete = 0;
+
+    protected void init_sourceDoc_Sence()
+    {        
+        class_Data_SqlSPEntry activeSPEntry_configSence = Object_CommonData.GetActiveSP(Object_CommonData.dbServer, class_SPSMap.SP_OPERATION_CONFIG_SENCE);
+        DataTable textDataTable = Object_CommonData.Object_SqlHelper.ExecuteSelectSPForDT(activeSPEntry_configSence, Object_CommonData.Object_SqlConnectionHelper, Object_CommonData.dbServer);
+        foreach (DataRow activeRow in textDataTable.Rows)
+        {
+            string symbol = string.Empty;
+            class_Data_SqlDataHelper.GetColumnData(activeRow, "symbol", out symbol);
+            symbolList.Add(symbol);
+        }
+    }
+
+    protected void init_sourceDoc_Profile()
+    {
+        string profileSymbol = "profile_" + "ikcoder_" + logined_user_name;
+        string requestAPI = "/Profile/api_AccountProfile_SelectProfileBySymbol.aspx?symbol=" + profileSymbol;
+        string URL = Server_API + Virtul_Folder_API + requestAPI;
+        string returnStrDoc = Object_NetRemote.getRemoteRequestToStringWithCookieHeader("<root></root>", URL, 1000 * 60, 100000);
+        XmlDocument tmpData = new XmlDocument();
+        tmpData.LoadXml(returnStrDoc);
+        if (tmpData.SelectSingleNode("/root/err") == null)
+        {
+            XmlNode msgNode = tmpData.SelectSingleNode("/root/msg");
+            sourceDoc_profile.LoadXml(class_XmlHelper.GetAttrValue(msgNode, "msg"));
+        }
+    }
+
+    protected void init_Honor()
+    {
+        class_Bus_Hornor objectHornor = new class_Bus_Hornor();
+        honorResult = objectHornor.get_HornorConfig(symbolList);
+    }
+
+    protected void set_Honor()
+    {
+        XmlNode honorNode = class_XmlHelper.CreateNode(centerDocument, "honor", "");
+        rootNode.AppendChild(honorNode);
+        foreach(Dictionary<string,string> activeItem in honorResult)
+        {
+            XmlNode newItem = class_XmlHelper.CreateNode(centerDocument, "item", "");
+            honorNode.AppendChild(newItem);
+            foreach (string keyName in activeItem.Keys)
+                class_XmlHelper.SetAttribute(newItem, keyName, activeItem[keyName]);
+        }
+    }
+
+    protected void init_Course()
+    {     
+        foreach (string activeSymbol in symbolList)
+        {
+            if (activeSymbol.StartsWith("a") && activeSymbol.StartsWith("A"))
+                count_primerSence++;
+            else if (activeSymbol.StartsWith("b") && activeSymbol.StartsWith("B"))
+                count_primarySence++;
+            else if (activeSymbol.StartsWith("c") && activeSymbol.StartsWith("C"))
+                count_middleSence++;
+            else if (activeSymbol.StartsWith("d") && activeSymbol.StartsWith("D"))
+                count_seniorSence++;
+            else
+                count_advanceSence++;
+        }
+        XmlNode finishedSenceNode = sourceDoc_profile.SelectSingleNode("/root/studystatus/finished");
+        if (finishedSenceNode != null)
+        {
+            XmlNodeList newItemNodes = sourceDoc_profile.SelectNodes("item");
+            
+        }
+    }
+
+    protected void set_Course()
+    {
+        XmlNode courseNode = class_XmlHelper.CreateNode(centerDocument, "course", "");
+        rootNode.AppendChild(courseNode);
+       
+    }
+  
     protected override void ExtendedAction()
     {
-        centerDocument.LoadXml("<root></root>");    
+        Object_CommonData.PrepareDataOperation();
+        init_sourceDoc_Sence();
+        init_sourceDoc_Profile();
+        init_Honor();
+        init_Course();
+        
+        centerDocument.LoadXml("<root></root>");
+        rootNode = centerDocument.SelectSingleNode("/root");
+        set_Honor();
     }
 }
+
