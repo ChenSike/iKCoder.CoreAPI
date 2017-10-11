5a73e12cb483843b2393db10e3eeede7635f6a51
sike.chen <sike.chen@morningstar.com>
2017-10-11 10:52:21 +0800

    new

---
+++ SourceCode/IKCoderAPI/Account/Common/api_iKCoder_Common_Get_SignStatus.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Account_User_api_iKCoder_User_Get_SignStatus : class_WebBase_IKCoderAPI_UA
+{
+    protected override void ExtendedAction()
+    {
+        
+    }
+}
