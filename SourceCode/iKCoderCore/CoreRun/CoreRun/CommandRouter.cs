ae6fbb34655946c2148646fab54f2014aef09472
sike.chen <sike.chen@morningstar.com>
2018-03-12 13:59:29 +0800

    NEW

---
+++ SourceCode/iKCoderCore/CoreRun/CoreRun/CommandRouter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CoreRun
+{
+
+    public class CommandItem
+    {
+        public string command;
+    }
+
+    public class CommandRouter
+    {
+        private Queue<CommandItem> CommandQueue = new Queue<CommandItem>();
+        private Thread ThreadProcess = null;
+
+        public void Set_PureCommand(string command)
+        {
+            CommandItem newItem = new CommandItem();
+            newItem.command = command;
+            CommandQueue.Enqueue(newItem);
+        }
+
+        public void Start_Process()
+        {
+            this.ThreadProcess = new Thread(new ThreadStart(Process));
+        }
+
+        private void Process()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ SourceCode/iKCoderCore/Sdk/iKCoderSDK/Basic/Basic_Exceptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace iKCoderSDK.Basic
+{
+    public class Basic_Exceptions:ApplicationException
+    {
+
+    }
+}
