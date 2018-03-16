860c3f4d7f1ea54f8b77fb7c77ee8444d6d9b3d9
sike.chen <sike.chen@morningstar.com>
2018-03-16 14:20:50 +0800

    new code

---
+++ SourceCode/iKCoderCore/CoreInit/CoreInit/Modules/M_Config.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using iKCoderSDK;
+
+namespace CoreInit
+{
+    public class M_Config:M_Base
+    {
+        private Basic_Config configDoc = new Basic_Config();
+
+        public string AppConfigFile
+        {
+            set;
+            get;
+        }
+
+        public override void Input()
+        {
+            Console.Write("M_Config@application configdoc file:");
+            this.AppConfigFile = Console.ReadLine();
+        }
+
+        public bool init()
+        {
+            return configDoc.DoOpen(this.AppConfigFile);
+        }
+
+        public void DESMode()
+        {
+            configDoc.SwitchToDESModeON();
+        }
+
+        public void NormalMode()
+        {
+            configDoc.SwitchToDESModeOFF();
+        }
+
+        public bool newSession(string sessionname,string sessionvalue)
+        {
+            configDoc.CreateNewSession()
+        }
+        
+
+    }
+}
