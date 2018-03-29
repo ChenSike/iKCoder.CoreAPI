9f4199a59ceb0d234f81ae4d4e61bc298c9bd306
sike.chen <sike.chen@outlook.com>
2018-03-29 23:15:48 +0800

    new

---
+++ SourceCode/iKCoderCore/CoreM/CoreM/Modules/M_CheckStore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using iKCoderSDK;
+using System.Data;
+using System.Xml;
+using System.Threading;
+
+namespace CoreM.Modules
+{
+    public class M_CheckStore
+    {
+        class_Data_SqlConnectionHelper db_objectConnectionHelper = new class_Data_SqlConnectionHelper();
+        Data_dbSqlHelper db_objectSqlHelper = new Data_dbSqlHelper();
+        Dictionary<String, class_Data_SqlSPEntry> Map_SPS = new Dictionary<string, class_Data_SqlSPEntry>();
+        Thread currentThread;
+
+        public void Init(Basic_Config refConfigObject)
+        {
+            XmlNode sessionNode = refConfigObject.GetSessionNode("coreMDB");
+            string server = refConfigObject.GetAttrValue(sessionNode, "server");
+            string uid = refConfigObject.GetAttrValue(sessionNode, "uid");
+            string pwd = refConfigObject.GetAttrValue(sessionNode, "pwd");
+            string db = refConfigObject.GetAttrValue(sessionNode, "db");
+            ConsoleMessageItem newMessageItem = new ConsoleMessageItem();
+            newMessageItem.Message = "[Connect to store database]";
+            newMessageItem.Result = true;
+            Program.obj_message.set_newMessage(newMessageItem);
+            db_objectConnectionHelper.Set_NewConnectionItem(Program.key_db_ikcoder_store, server, uid, pwd, db, enum_DatabaseType.MySql);
+            newMessageItem.Message = "[Loading SPS for store]";
+            newMessageItem.Result = true;
+            Program.obj_message.set_newMessage(newMessageItem);
+            Map_SPS = db_objectSqlHelper.ActionAutoLoadingAllSPS(db_objectConnectionHelper.Get_ActiveConnection(Program.key_db_ikcoder_store), "");
+            
+        }
+
+        public void Start()
+        {
+            class_Data_SqlSPEntry = Map_SPS[""];
+            while(true)
+            {
+                
+            }
+        }
+
+
+
+    }
+}
--- SourceCode/iKCoderCore/CoreM/CoreM/Program.cs
+++ SourceCode/iKCoderCore/CoreM/CoreM/Program.cs
@@ -1,26 +1,27 @@
 ﻿using System;
 using iKCoderSDK;
 using System.Collections.Generic;
+using System.Xml;
 
 namespace CoreM
 {
     class Program
     {
 
+        public static string key_db_ikcoder_store = "ikcoder_store";
+        public static iKCoderSDK.Basic_Config coreMconfig = new Basic_Config();
+        public static ConsoleMessage obj_message = new ConsoleMessage();
+
         static void Main(string[] args)
         {
-            iKCoderSDK.Basic_Config coreMconfig = new Basic_Config();
-            coreMconfig.DoOpen("coreMconfig.xml");
-            Dictionary<string, Dictionary<string, string>> Map_ApiConfigs = new Dictionary<string, Dictionary<string, string>>();
-            Dictionary<String, class_Data_SqlSPEntry> Map_SPS = new Dictionary<string, class_Data_SqlSPEntry>();
-            class_Data_SqlConnectionHelper db_objectConnectionHelper = new class_Data_SqlConnectionHelper();
-            Data_dbSqlHelper db_objectSqlHelper = new Data_dbSqlHelper();
-            ConsoleMessage obj_message = new ConsoleMessage();
-            obj_message.start_process();
             Console.WriteLine("CoreM 1.0 2018 copyright by iKCoder.LTD.ShenZhen.");
             Console.WriteLine("Start services...");
             Console.WriteLine("[CoreM will keep running until this process closed.]");
-
+            obj_message.start_process();
+            coreMconfig.DoOpen("coreMconfig.xml");
+            Console.WriteLine("[Load module:M_CheckStore]");
+                       
+            
         }
     }
 }
